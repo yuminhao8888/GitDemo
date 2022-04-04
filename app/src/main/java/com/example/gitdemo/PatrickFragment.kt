f513462574e6dd3188382ec56a71679b9c35e2e0
consultant <ymhao8888@gmail.com>
2022-04-03 20:16:12 -0400

    update PatrickFragment

--- app/src/main/java/com/example/gitdemo/PatrickFragment.kt
+++ app/src/main/java/com/example/gitdemo/PatrickFragment.kt
@@ -5,21 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.gitdemo.databinding.FragmentPatrickBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PatrickFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+
 class PatrickFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private val binding by lazy {
+        FragmentPatrickBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +35,10 @@ class PatrickFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding.textView.text = "This fragment is from feature/Patrick branch"
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_patrick, container, false)
+        return binding.root
     }
 
     companion object {
